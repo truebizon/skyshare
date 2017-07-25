c57a36876667559d2a3a894e958e09e1b8d1be84
Mamoru Masumoto <Mark@new-MacBook.local>
2017-07-25 21:57:33 +0900

    After migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725092958) do
+ActiveRecord::Schema.define(version: 20170725120822) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "note",       limit: 65535, null: false
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -20,5 +27,35 @@ ActiveRecord::Schema.define(version: 20170725092958) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id",       limit: 4,     null: false
+    t.string   "title",         limit: 255,   null: false
+    t.text     "note",          limit: 65535, null: false
+    t.text     "product_img",   limit: 65535
+    t.text     "product_video", limit: 65535
+    t.integer  "rate",          limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "title",                  limit: 255,              null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
